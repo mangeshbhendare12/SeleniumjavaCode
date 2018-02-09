0d69c51cac65b590e400f6d3ea2d73bca157abad
bhendman <bhendman@CSNB0291.ap.dtcs.local>
2018-02-09 12:45:49 +0530

    Updated Myclass.java

--- WebtestIngClass/src/com/MyClass.java
+++ WebtestIngClass/src/com/MyClass.java
@@ -5,7 +5,7 @@ public class MyClass {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		ABC ob = new ABC();
-		// ob.T();
+		ob.T();
 		ob.T2();
 	}
 
